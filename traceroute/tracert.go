bc10cb14395ce908cc96d02db28b4a37618efaf7
Olivier Poitrey <rs@nextdns.io>
2020-11-16 23:35:31 +0100

    Improve tracert parser

--- traceroute/tracert.go
+++ traceroute/tracert.go
@@ -3,26 +3,32 @@ package traceroute
 import (
 	"fmt"
 	"net"
+	"strings"
 	"time"
 )
 
 func parseTracertLine(line string) (Hop, bool) {
 	var seq, rtt1, rtt2, rtt3 int
 	var ip string
+	line = strings.ReplaceAll(line, "*", "-1 ms")
 	_, err := fmt.Sscanf(line, "%d %d ms %d ms %d ms %s", &seq, &rtt1, &rtt2, &rtt3, &ip)
 	if err != nil {
 		return Hop{}, false
 	}
 	nip := net.ParseIP(ip)
-	if nip == nil {
-		return Hop{}, false
-	}
 	return Hop{
 		Seq: seq,
 		Info: []HopInfo{
-			{nip, time.Duration(rtt1) * time.Millisecond},
-			{nip, time.Duration(rtt2) * time.Millisecond},
-			{nip, time.Duration(rtt3) * time.Millisecond},
+			{nip, tracerMsToDuration(rtt1)},
+			{nip, tracerMsToDuration(rtt2)},
+			{nip, tracerMsToDuration(rtt3)},
 		},
 	}, true
+}
+
+func tracerMsToDuration(ms int) time.Duration {
+	if ms == -1 {
+		return -1
+	}
+	return time.Duration(ms) * time.Millisecond
 }
--- traceroute/tracert_test.go
+++ traceroute/tracert_test.go
@@ -25,6 +25,50 @@ func Test_parseTracertLine(t *testing.T) {
 				},
 			},
 			true},
+		{"Success v6",
+			"  2     4 ms     3 ms     3 ms  2a01:e00:2b:f836:af9c::ffff",
+			Hop{
+				Seq: 2,
+				Info: []HopInfo{
+					{net.ParseIP("2a01:e00:2b:f836:af9c::ffff"), 4 * time.Millisecond},
+					{net.ParseIP("2a01:e00:2b:f836:af9c::ffff"), 3 * time.Millisecond},
+					{net.ParseIP("2a01:e00:2b:f836:af9c::ffff"), 3 * time.Millisecond},
+				},
+			},
+			true},
+		{"Full Timeout v4",
+			"3     *        *        *     Request timed out.",
+			Hop{
+				Seq: 3,
+				Info: []HopInfo{
+					{nil, -1},
+					{nil, -1},
+					{nil, -1},
+				},
+			},
+			true},
+		{"Partial Timeout v4",
+			"  1     8 ms     *    10 ms  45.90.28.0",
+			Hop{
+				Seq: 1,
+				Info: []HopInfo{
+					{net.ParseIP("45.90.28.0"), 8 * time.Millisecond},
+					{net.ParseIP("45.90.28.0"), -1},
+					{net.ParseIP("45.90.28.0"), 10 * time.Millisecond},
+				},
+			},
+			true},
+		{"Partial Timeout v6",
+			"  8     *        *       17 ms  2001:550:0:1000::8275:319a",
+			Hop{
+				Seq: 8,
+				Info: []HopInfo{
+					{net.ParseIP("2001:550:0:1000::8275:319a"), -1},
+					{net.ParseIP("2001:550:0:1000::8275:319a"), -1},
+					{net.ParseIP("2001:550:0:1000::8275:319a"), 17 * time.Millisecond},
+				},
+			},
+			true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
